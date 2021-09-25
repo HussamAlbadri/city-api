531e387ca8d0558dd9dc16f639d8dc397cbaab7b
Hussam Albadri <hussamalbadri2@gmail.com>
2021-09-25 20:30:15 +0300

    back end

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ server.get('/', (req, res) => {
     res.status(200).send('home route')
 })
 const weatherInformation = require('./data/weatherApi.json')
-server.get('/test', (request, response) => {
-    response.send('api server is working')
-})
 
 class WeatherApi {
-    constructor(item) {
-        this.data = item.datetime;
-        this.descrip = item.weather.description;
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
     }
 
 }
@@ -28,17 +25,17 @@ class WeatherApi {
 server.get('/getweatherApi', (req, res) => {
     try {
         let weatherArea = req.query.city;
-        let cityData = weatherInformation.find((data) => {
-            if (data.city_name === weatherArea) {
-                return data;
+        let cityData = weatherInformation.find((item) => {
+            if (item.city_name === weatherArea) {
+                return item;
             }
         });
-        let array = cityData.data.map(item => {
-            return new WeatherApi(item);
+        let array = cityData.data.map(ele => {
+            return new WeatherApi(ele.datetime, ele.weather.description);
         });
-        res.status(200).send(array);
+        res.send(array);
     } catch (err) {
-        res.status(404).send("Error: Something Went Wrong \n The Site Is Not Avaible");
+        res.send("Error: Something Went Wrong \n The Site Is Not Avaible");
     }
 
 });
