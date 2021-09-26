4e73302168a70493055a504d8a5a1a4fcf4a40c2
Hussam Albadri <hussamalbadri2@gmail.com>
2021-09-27 01:55:50 +0300

    lab09

--- server.js
+++ server.js
@@ -15,69 +15,13 @@ server.use(cors());
 // })
 // const weatherInformation = require('./data/weatherApi.json')
 
+// Modules
+const getweatherinformation = require('./modules/weatheer.js')
+const getMovieData = require('./modules/moviees.js')
+
+// Roots
 server.get('/getweatherApi', getweatherinformation);
 server.get('/movies', getMovieData);
-
-
-class WeatherApi {
-    constructor(item) {
-        this.date = item.valid_date;
-        this.description = item.weather.description;
-    }
-
-}
-
-class MovieCon {
-    constructor(element) {
-        this.title = element.title;
-        this.overview = element.overview;
-        this.average_votes = element.vote_average;
-        this.total_votes = element.vote_count;
-        this.image_url = 'https://image.tmdb.org/t/p/w500' + element.poster_path;
-        this.popularity = element.popularity;
-        this.released_on = element.release_date;
-    }
-}
-
-function getweatherinformation(req, res) {
-    //http://localhost:3004/getweatherApi?city=Amman 
-
-    let weatherArea = req.query.city;
-
-    let URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weatherArea}&key=${process.env.WEATHER_KEY}`
-    console.log(URL)
-    try {
-        axios.get(URL).then(dataCollection => {
-            let array = dataCollection.data.data.map(item => {
-                return new WeatherApi(item)
-            })
-            res.send(array)
-        })
-    } catch (error) {
-        res.send(error);
-    }
-
-}
-
-function getMovieData(req, res) {
-    let movieHandler = req.query.city
-        //http://localhost:3004/movies?city=Amman 
-
-    let movieLink = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${movieHandler}`
-        // console.log(movieLink)
-    try {
-        axios.get(movieLink).then(dataCollectionResult => {
-            let array = dataCollectionResult.data.results.map(element => {
-                return new MovieCon(element)
-            })
-            res.send(array)
-        });
-
-    } catch (error) {
-        res.send(error);
-    }
-}
-
 
 // localhost:3005/ANYTHING
 server.get('*', (req, res) => {
